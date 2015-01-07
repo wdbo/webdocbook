3213aaf70d0586c02c925dd4d809ce41d39613a6
piwi <me@e-piwi.fr>
2015-01-07 10:54:26 +0100

    review of the interface following #a393f15

--- www/index.php
+++ www/index.php
@@ -22,19 +22,29 @@
  */
 
 /**
- * Show errors at least initially
+ * Application configuration file
+ */
+define('DOCBOOK_CONFIG_FILE', __DIR__.'/../src/config/docbook.ini');
+
+/**
+ * Application mode: 'dev' or anything else (as prod)
+ */
+define('DOCBOOK_MODE', 'dev');
+
+/**
+ * Show errors at least initially in dev mode
  *
  * `E_ALL` => for hard dev
  * `E_ALL & ~E_STRICT` => for hard dev in PHP5.4 avoiding strict warnings
  * `E_ALL & ~E_NOTICE & ~E_STRICT` => classic setting
  */
-@ini_set('display_errors','1'); @error_reporting(E_ALL);
-//@ini_set('display_errors','1'); @error_reporting(E_ALL & ~E_STRICT);
-//@ini_set('display_errors','1'); @error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+@ini_set('display_errors',1); @error_reporting(E_ALL);
+//@ini_set('display_errors',1); @error_reporting(E_ALL & ~E_STRICT);
+//@ini_set('display_errors',1); @error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 
 // Set a default timezone to avoid PHP5 warnings
 $dtmz = @date_default_timezone_get();
-@date_default_timezone_set( $dtmz?:'Europe/London' );
+@date_default_timezone_set($dtmz?:'Europe/London');
 
 // Get Composer autoloader
 $composerAutoLoader = __DIR__.'/../src/vendor/autoload.php';
@@ -45,10 +55,14 @@ if (@file_exists($composerAutoLoader)) {
     ." (see: <a href=\"http://getcomposer.org/doc/00-intro.md#using-composer\">http://getcomposer.org/doc/00-intro.md#using-composer</a>)!");
 }
 
-// uncomment in dev mode
-define('DOCBOOK_MODE', 'dev');
+// silent errors in production mode
+if (!defined('DOCBOOK_MODE') || DOCBOOK_MODE!=='dev') {
+    @ini_set('display_errors',0);
+}
 
-// the application 
-\DocBook\FrontController::getInstance()->distribute();
+// the application
+\DocBook\FrontController::getInstance(
+    defined('DOCBOOK_CONFIG_FILE') ? DOCBOOK_CONFIG_FILE : __DIR__.'/../src/config/docbook.ini'
+)->distribute();
 
 // Endfile
